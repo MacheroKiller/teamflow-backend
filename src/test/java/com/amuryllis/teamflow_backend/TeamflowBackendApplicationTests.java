eca9000f54825c2250a3e232cfb598f9ed7dc44d
MacheroKiller <stevan1533@gmail.com>
2026-08-29 19:19:00 -0500

    chore: initial Spring Boot 4.1.0 project scaffold (Java 21, Maven)

---
+++ src/test/java/com/amuryllis/teamflow_backend/TeamflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amuryllis.teamflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
